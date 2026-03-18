8f17db585388558356df2508954dfff3699115cb
jhonnftdev <jhonnftx21@gmail.com>
2026-03-18 08:32:58 -0500

    configuración (modelo,  controlador, ruta inicial

---
+++ app/Http/Controllers/DireccionController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Direccion;
+
+class DireccionController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DireccionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DireccionController::class, 'index'])->name('home');
