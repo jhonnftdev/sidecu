d3ae54d4848a270d0bb02298b71b34edd118eb1d
jhonnftdev <jhonnftx21@gmail.com>
2026-03-18 08:23:42 -0500

    Configuracion inicial_estructura base

---
+++ database/migrations/2026_03_18_130100_create_direcciones_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('direcciones', function (Blueprint $table) {
+    $table->id();
+    $table->string('canton', 100);
+    $table->string('calle_principal', 150);
+    $table->string('calle_secundaria', 150)->nullable();
+    $table->text('referencia');
+    $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('direcciones');
+    }
+};
